ffdae6aec3719ab8080f28794c857e1c33b2299a
frostbane <frostbane@programmer.net>
2021-07-10 00:03:28 +1000

    move the main include file

---
+++ src/injectpp.hpp
@@ -0,0 +1,15 @@
+#ifndef INJECTPP_HPP_HEADER__
+#define INJECTPP_HPP_HEADER__
+
+#include "injectpp/injectppcommon.hpp"
+#include "injectpp/typeid.hpp"
+#include "injectpp/typemap.hpp"
+#include "injectpp/abstractinstancecontainer.hpp"
+#include "injectpp/instancecontainer.hpp"
+#include "injectpp/injector.hpp"
+#include "injectpp/config.hpp"
+
+
+
+#endif /* INJECTPP_HPP_HEADER__ */
+
